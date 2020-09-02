f226393a31770e95960b9f46de5f650c7fdd3457
leeyunhome <leeyunhome@daum.net>
2020-09-02 20:42:16 +0900

    assign two minus value to glViewport as first two parameter

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 const int width_window = 640;
 const int height_window = 480;
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 int main(void)
 {
 	GLFWwindow *window = nullptr;
@@ -28,7 +29,7 @@ int main(void)
 	}
 
 	// callbacks here
-	
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
@@ -36,7 +37,7 @@ int main(void)
 
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+	glViewport(-50, -50, width, height);
 	// glOrtho(0, 1, 0, 1, -1.0, 1.0);
 	const float aspect_ratio = (float)width/(float)height;
 	glOrtho(-1, 1, -1 / aspect_ratio, 1 / aspect_ratio, -1.0, 1.0);
@@ -49,16 +50,22 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//TODO: draw here
-		glColor3f(1.0, 0.0, 0.0);
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.0, 0.0, 0.0);
-		glVertex3f(0.0, 0.0, 0.0);
+		glBegin(GL_TRIANGLE_FAN);
+		glColor3f(0.5, 0.0, 0.0);
+		// center of polygonized circle
+		glVertex2f(0.0, 0.0);
 
-		glColor3f(0.0, 1.0, 0.0);
-		glVertex3f(0.5, 0.0, 0.0);
+		const int num_triangles = 1000;
+		const float dtheta = 2.0 * 3.141592	/ (float)num_triangles;
+		const float radius = 0.5;
 
-		glColor3f(0.0, 0.0, 1.0);
-		glVertex3f(0.25, 0.5, 0.0);
+		float theta = 0.0;
+		for (int i = 0; i <=num_triangles; i++, theta += dtheta)
+		{
+			const float x = radius * cos(theta);
+			const float y = radius * sin(theta);
+			glVertex2f(x, y);
+		}
 
 		glEnd();
 		/* Swap front and back buffers */
@@ -75,3 +82,10 @@ int main(void)
 	return 0;
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+	{
+		std::cout << "button clicked" << std::endl;
+	}
+}
